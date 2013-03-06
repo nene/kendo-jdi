b6103542f542efc70a11478b26b326dd9e2c991e
Rene Saarsoo <nene@triin.net>
2013-03-05 18:16:43 -0800

    Create database if doesn't exist yet.

--- data.php
+++ data.php
@@ -1,12 +1,22 @@
 <?php
 $db = new PDO('sqlite:sample.db');
-?>
-<?php
+
+// When our table doesn't yet exist
+if (!$db->query('SELECT * FROM task LIMIT 1')) {
+  $db->exec('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, label TEXT, date DATE, duration INT, done SMALLINT);');
+  // pull in data
+  $data = json_decode(file_get_contents("data.json"), true);
+  $insert = $db->prepare("INSERT INTO task (label, date, duration, done) VALUES (:label, :date, :duration, :done)");
+  foreach ($data as $row) {
+    $r = array(":label" => $row["label"], ":date" => $row["date"], ":duration" => $row["duration"], ":done" => $row["done"]);
+    $insert->execute($r);
+  }
+}
+
 $statement = $db->prepare('SELECT * FROM task');
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
-?>
-<?php
+
 // Set response content type
 header('Content-Type: application/json');
 
