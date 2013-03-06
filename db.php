c03dfe1f333f7b027fb9f4d7f8b4ae8d8f082875
Rene Saarsoo <nene@triin.net>
2013-03-05 18:34:17 -0800

    Wrap database initialization inside function.

--- db.php
+++ db.php
@@ -1,15 +1,19 @@
 <?php
 // Establish database connection.
-$db = new PDO('sqlite:sample.db');
 
-// When our table doesn't yet exist
-if (!$db->query('SELECT * FROM task LIMIT 1')) {
-  $db->exec('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, label TEXT, date DATE, duration INT, done SMALLINT);');
-  // pull in data
-  $data = json_decode(file_get_contents("data.json"), true);
-  $insert = $db->prepare("INSERT INTO task (label, date, duration, done) VALUES (:label, :date, :duration, :done)");
-  foreach ($data as $row) {
-    $r = array(":label" => $row["label"], ":date" => $row["date"], ":duration" => $row["duration"], ":done" => $row["done"]);
-    $insert->execute($r);
+// Creates our tasks table when it doesn't exist yet.
+function init_database($db) {
+  if (!$db->query('SELECT * FROM task LIMIT 1')) {
+    $db->exec('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, label TEXT, date DATE, duration INT, done SMALLINT);');
+    // pull in data
+    $data = json_decode(file_get_contents("data.json"), true);
+    $insert = $db->prepare("INSERT INTO task (label, date, duration, done) VALUES (:label, :date, :duration, :done)");
+    foreach ($data as $row) {
+      $r = array(":label" => $row["label"], ":date" => $row["date"], ":duration" => $row["duration"], ":done" => $row["done"]);
+      $insert->execute($r);
+    }
   }
 }
+
+$db = new PDO('sqlite:sample.db');
+init_database($db);
