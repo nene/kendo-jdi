f2158a339940634eb12e6f28f8d5019b2239145d
Rene Saarsoo <nene@triin.net>
2013-03-05 18:31:28 -0800

    Refactor database setup to separate file.

--- create_task.php
+++ create_task.php
@@ -1,4 +1,5 @@
 <?php
+require_once("db.php");
 
 //read from POST
 $json = @file_get_contents('php://input');
@@ -10,25 +11,6 @@ $label = $obj->{'label'};//'Buy milk';
 $date = $obj->{'date'};//'2013-03-06';
 $duration = $obj->{'duration'};//'5';
 $done = '0';// not done when created, ignore json
-
-// database connection
-
-// create
-$db = new PDO("sqlite:sample.db");
-
-//TODO factor this out into its own file, along with from data.php
-// When our table doesn't yet exist
-if (!$db->query('SELECT * FROM task LIMIT 1')) {
-  $db->exec('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, label TEXT, date DATE, duration INT, done SMALLINT);');
-  // pull in data
-  $data = json_decode(file_get_contents("data.json"), true);
-  $insert = $db->prepare("INSERT INTO task (label, date, duration, done) VALUES (:label, :date, :duration, :done)");
-  foreach ($data as $row) {
-    $r = array(":label" => $row["label"], ":date" => $row["date"], ":duration" => $row["duration"], ":done" => $row["done"]);
-    $insert->execute($r);
-  }
-}
-
 
 
 // query
--- data.php
+++ data.php
@@ -1,17 +1,5 @@
 <?php
-$db = new PDO('sqlite:sample.db');
-
-// When our table doesn't yet exist
-if (!$db->query('SELECT * FROM task LIMIT 1')) {
-  $db->exec('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, label TEXT, date DATE, duration INT, done SMALLINT);');
-  // pull in data
-  $data = json_decode(file_get_contents("data.json"), true);
-  $insert = $db->prepare("INSERT INTO task (label, date, duration, done) VALUES (:label, :date, :duration, :done)");
-  foreach ($data as $row) {
-    $r = array(":label" => $row["label"], ":date" => $row["date"], ":duration" => $row["duration"], ":done" => $row["done"]);
-    $insert->execute($r);
-  }
-}
+require_once("db.php");
 
 $statement = $db->prepare('SELECT * FROM task');
 $statement->execute();
