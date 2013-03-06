9b464ffa01edeafc3221dbd94b3782bd6fd60c5d
Colin Alworth <niloc132@gmail.com>
2013-03-05 20:20:12 -0600

    Added stolen db create code

--- create_task.php
+++ create_task.php
@@ -12,15 +12,24 @@ $duration = $obj->{'duration'};//'5';
 $done = '0';// not done when created, ignore json
 
 // database connection
-//TODO factor this out into its own file, along with from data.php
-
-
-
-
-
 
 // create
 $db = new PDO("sqlite:sample.db");
+
+//TODO factor this out into its own file, along with from data.php
+// When our table doesn't yet exist
+if (!$db->query('SELECT * FROM task LIMIT 1')) {
+  $db->exec('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, label TEXT, date DATE, duration INT, done SMALLINT);');
+  // pull in data
+  $data = json_decode(file_get_contents("data.json"), true);
+  $insert = $db->prepare("INSERT INTO task (label, date, duration, done) VALUES (:label, :date, :duration, :done)");
+  foreach ($data as $row) {
+    $r = array(":label" => $row["label"], ":date" => $row["date"], ":duration" => $row["duration"], ":done" => $row["done"]);
+    $insert->execute($r);
+  }
+}
+
+
 
 // query
 $sql = "INSERT INTO task (label,date,duration,done) VALUES (:label,:date,:duration,:done)";
